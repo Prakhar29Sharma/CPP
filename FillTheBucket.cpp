cb9c8ed68eb3a5fff0f638478794980426a1e29c
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-08-09 23:17:53 +0530

    FillTheBucked Codechef problem

---
+++ FillTheBucket.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void test()
+{
+	//code here
+	int K, X;
+	scanf("%d %d", &K, &X);
+	printf("%d\n", K-X);
+	return;
+}
+
+int main(){
+	int T;
+	scanf("%d", &T);
+	for(int i = 0; i < T; i++)
+	{
+		test();
+	}
+	return 0;
+}
