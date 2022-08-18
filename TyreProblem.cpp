caae10a48488696f446331696258f215feed534e
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-08-19 00:02:46 +0530

    codechef TyreProblem

---
+++ TyreProblem.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void test()
+{
+	//code here
+	int N, M, total;
+	scanf("%d %d", &N, &M);
+	total = N*2 + M*4;
+	printf("%d\n", total);
+	
+	return;
+}
+
+int main(){
+	int T;
+	scanf("%d", &T);
+	for(int i = 0; i < T; i++)
+	{
+		test();
+	}
+	return 0;
+}
