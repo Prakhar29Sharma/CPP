bdc34d5c3b23a77c0e84bbca9631c77121eeb62c
Prakhar Sharma <prakhar29sharma@gmail.com>
2022-08-23 09:11:00 +0530

    Added Menu for Queue ops

--- CircularQueue.cpp
+++ CircularQueue.cpp
@@ -34,14 +34,15 @@ class Queue {
 				arr[rear] = value;
 			}
 		}
-		int dequeue()
+		void dequeue()
 		{
 			if(front == -1){
 				printf("Queue is Empty!");
-				return -1;
+				return;
 			}
 			
-			int data = arr[front];
+			printf("\n%d is dequeued", arr[front]);
+			
 			arr[front] = -1;
 			if(front == rear)
 			{
@@ -55,9 +56,7 @@ class Queue {
 			{
 				front++;
 			}
-				
-			return data;
-
+			return;
 		}
 		void displayQueue()
 		{
@@ -89,14 +88,37 @@ int main()
 {
 	Queue q(5);
 
-	q.enqueue(14);
-	q.enqueue(45);
-	q.enqueue(1);
-	q.enqueue(48);
-	q.enqueue(56);
-		
-	q.displayQueue();
+	int ch = 0, value;
 	
-	q.enqueue(56);
+	do {
+		printf("\n----Queue Menu----");
+		printf("\n1.Enqueue");
+		printf("\n2.Dequeue");
+		printf("\n3.Show");
+		printf("\n4.Exit");
+		printf("\nEnter your choice : ");
+		scanf("%d", &ch);
+		switch(ch)
+		{
+			case 1:
+				printf("\nEnter a value : ");
+				scanf("%d", &value);
+				q.enqueue(value);
+				break;
+			case 2:
+				q.dequeue();
+				break;
+			case 3:
+				q.displayQueue();
+				break;
+		}
+		
+		if(ch < 1 || ch > 4)
+		{
+			printf("\nInvalid Choice! Try Again!");
+		}
+		
+	}while(ch!=4);
+	
 	return 0;	
 }
